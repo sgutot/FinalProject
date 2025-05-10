b58630d2abe254ba84f1c22041d53508f92c80ec
Fantasya63 <baloaloajansen.school@gmail.com>
2025-05-10 20:35:04 +0800

    all pyc files removed

--- server/settings.py
+++ server/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +138,5 @@ CORS_ALLOWED_ORIGIN = [
     "http://localhost:5173",
 ]
 
+# Disable this in production
+CORS_ALLOW_ALL_ORIGINS = True
